f68072e54e2203bd74b362461493687c6589a457
Miquel Roig <miquel.roig@urv.cat>
2021-01-11 09:21:04 +0100

    try2

---
+++ ConsoleApp53/ConsoleApp53/Empleat.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp53
+{
+    public class Empleat
+    {
+        private double _sou;
+
+        public double Sou
+        {
+            get { return _sou; }
+            set { _sou = value; }
+        }
+
+        public Empleat(int s)
+        {
+            Sou = s;
+        }
+
+        public void bonus()
+        {
+            double op = Sou * 1.1;
+            Sou = op;
+        }
+
+        public virtual double calcularSou()
+        {
+            return Sou;
+        }
+
+        public virtual double calcularNet()
+        {
+            return Sou;
+        }
+    }
+
+    public class Boss : Empleat
+    {
+        public Boss(int s)
+            : base (s) 
+        {
+           
+        }
+
+        public override double calcularSou()
+        {
+            double op;
+            op = base.calcularSou() * 1.5;
+            return op;
+        }
+
+        public override double calcularNet()
+        {
+            double op;
+            op = base.calcularNet() * 1.5 * 0.68;
+            return op;
+        }
+    }
+
+    public class Manager : Empleat
+    {
+        public Manager(int s)
+            : base(s)
+        {
+
+        }
+
+        public override double calcularSou()
+        {
+            double op;
+            op = base.calcularSou() * 1.1;
+            return op;
+        }
+
+        public override double calcularNet()
+        {
+            double op;
+            op = base.calcularNet() * 1.1 * 0.74;
+            return op;
+        }
+    }
+
+    public class Empoyee : Empleat
+    {
+        public Empoyee(int s)
+            : base(s)
+        {
+
+        }
+
+        public override double calcularSou()
+        {
+            double op;
+            op = base.calcularSou() * 0.85;
+            return op;
+        }       
+    }
+
+    public class Volunteer : Empleat
+    {
+        private bool ajuda;
+        private double cobrar;
+        public Volunteer(int s)
+            : base(s)
+        {
+            ajuda = false;
+            cobrar = 0;
+        }
+
+        public void rep_ajuda(int num)
+        {
+            ajuda = true;
+            cobrar = num;
+        }
+
+        public override double calcularSou()
+        {
+            double op;
+            op = base.calcularSou() * 0;
+            if (ajuda)
+            {
+                if (cobrar > 300)
+                    throw new InvalidSalaryException("L'ajuda ha superat els 300");
+                else
+                    op = cobrar;
+            }
+            return op;
+        }
+    }
+
+    class InvalidSalaryException : Exception
+    {
+        public InvalidSalaryException(string e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ ConsoleApp53/ConsoleApp53/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        public class Empleat
+        {
+            private int _sou;
+
+            public int Sou
+            {
+                get { return _sou; }
+                set { _sou = value; }
+            }
+
+            public Empleat(int s)
+            {
+                Sou = s;
+            }
+
+            public int calcularSou()
+            {
+                return Sou;
+            }  
+            
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
